2fbeba09ddcfb03db0e866ae81778d407943323d
bruce <bruce@hide.com>
2019-10-17 18:59:03 +0800

    modify shell script.

--- build_and_run_in_avd.sh
+++ build_and_run_in_avd.sh
@@ -2,9 +2,20 @@
 
 ndk-build clean > /dev/null # dont show any logs.
 ndk-build NDK_DEBUG=1 > /dev/null
-
-adb push ./libs/armeabi/main /data/local/ > /dev/null
-# next to execute 'main' in an avd, pls startup an arm avd. 
+fileurl="./libs/armeabi/main"
+if [ ! -f "$fileurl" ]
+then
+	   echo "$0: File '${fileurl}' not found."
+	   exit 1
+fi
+printf "\033[1;32m:)  built suc. \033[0m\n"
+echo " "
+sleep 2
+echo "find avd...."
+echo " "
+sleep 3
+adb push ${fileurl} /data/local/ > /dev/null
+# next to execute 'main' in an avd, pls startup an arm avd.
 adb shell "
 cd /data/local/
 ./main
