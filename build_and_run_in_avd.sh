53a57d8081e6286279551a2d16d6b9997a0b24e7
bruce <hide@hide.com>
2023-01-11 10:19:55 +0800

    code comment

--- build_and_run_in_avd.sh
+++ build_and_run_in_avd.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -e
+ndk-build clean >/dev/null # dont show any logs.
+ndk-build NDK_DEBUG=1 >/dev/null
 
-ndk-build clean > /dev/null # dont show any logs.
-ndk-build NDK_DEBUG=1 > /dev/null
+# in this line, I suppose that you have a arm emulator or devive connected. If that is
 fileurl="./libs/armeabi/main"
-if [ ! -f "$fileurl" ]
-then
-	   echo "$0: File '${fileurl}' not found."
-	   exit 1
+if [ ! -f "$fileurl" ]; then
+  echo "$0: File '${fileurl}' not found."
+  exit 1
 fi
 printf "\033[1;32m:)  built suc. \033[0m\n"
 echo " "
@@ -14,7 +15,7 @@ sleep 2
 echo "find avd...."
 echo " "
 sleep 3
-adb push ${fileurl} /data/local/ > /dev/null
+adb push ${fileurl} /data/local/ >/dev/null
 # next to execute 'main' in an avd, pls startup an arm avd.
 adb shell "
 cd /data/local/
